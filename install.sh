7868b82d77d8c060c3048f978534fc8134e797c9
Aleix Quintana Alsius <kinta@communia.org>
2021-12-01 12:01:08 +0100

    Update to 4.5

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-wget -O tizen_studio.bin https://download.tizen.org/sdk/Installer/tizen-studio_4.1.1/web-ide_Tizen_Studio_4.1.1_ubuntu-64.bin
+wget -O tizen_studio.bin http://usa.sdk-dl.tizen.org/web-ide_Tizen_Studio_4.5_usa_ubuntu-64.bin
 chmod +x tizen_studio.bin
 PATH=$(pwd):$PATH ./tizen_studio.bin
