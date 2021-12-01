544822731098523bea16064d41b2fa689d9295e1
Anton Palgunov <Toxblh@users.noreply.github.com>
2021-12-01 16:21:22 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-wget -O tizen_studio.bin http://usa.sdk-dl.tizen.org/web-ide_Tizen_Studio_4.5_usa_ubuntu-64.bin
+wget -O tizen_studio.bin https://download.tizen.org/sdk/Installer/tizen-studio_4.5/web-ide_Tizen_Studio_4.5_ubuntu-64.bin
+# For US CDN
+# wget -O tizen_studio.bin http://usa.sdk-dl.tizen.org/web-ide_Tizen_Studio_4.5_usa_ubuntu-64.bin
 chmod +x tizen_studio.bin
 PATH=$(pwd):$PATH ./tizen_studio.bin
